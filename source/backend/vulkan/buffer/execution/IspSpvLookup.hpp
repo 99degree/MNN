ce2a802fa083cdcef3229b933f9ba806f5e9b9ec
George Chan <gchan9527@gmail.com>
2026-08-14 22:29:03 +0800

    Fix IspSpvLookup: add all 50 isp.* ops to embedded lookup table
    The previous update only added 24 entries to the lookup table (the 14 HEAVY
    profile ops were added as headers but not to the map). This commit adds
    all remaining 26 ops to the lookup map for 50/50 total coverage.
    
    Also fixes:
    - Symbol visibility: headers use const (external linkage) with
      #pragma GCC visibility push(default) in .cpp for export
    - Symbol naming: all headers use g_isp_xxx_spv naming convention

--- source/backend/vulkan/buffer/execution/IspSpvLookup.hpp
+++ source/backend/vulkan/buffer/execution/IspSpvLookup.hpp
@@ -23,12 +23,40 @@ extern const unsigned char g_isp_calib_stats_spv[];
 extern const int g_isp_calib_stats_spv_len;
 extern const unsigned char g_isp_denoise_spv[];
 extern const int g_isp_denoise_spv_len;
+extern const unsigned char g_isp_eis_gyro_spv[];
+extern const int g_isp_eis_gyro_spv_len;
 extern const unsigned char g_isp_ispc_stats_spv[];
 extern const int g_isp_ispc_stats_spv_len;
 extern const unsigned char g_isp_tone_spv[];
 extern const int g_isp_tone_spv_len;
-extern const unsigned char g_isp_eis_gyro_spv[];
-extern const int g_isp_eis_gyro_spv_len;
+extern const unsigned char g_isp_normalize_spv[];
+extern const int g_isp_normalize_spv_len;
+extern const unsigned char g_isp_blc_spv[];
+extern const int g_isp_blc_spv_len;
+extern const unsigned char g_isp_cfa_spv[];
+extern const int g_isp_cfa_spv_len;
+extern const unsigned char g_isp_bayer_wb_spv[];
+extern const int g_isp_bayer_wb_spv_len;
+extern const unsigned char g_isp_demosaic_edge_spv[];
+extern const int g_isp_demosaic_edge_spv_len;
+extern const unsigned char g_isp_ccm_spv[];
+extern const int g_isp_ccm_spv_len;
+extern const unsigned char g_isp_downscale_spv[];
+extern const int g_isp_downscale_spv_len;
+extern const unsigned char g_isp_vignetting_spv[];
+extern const int g_isp_vignetting_spv_len;
+extern const unsigned char g_isp_saturation_spv[];
+extern const int g_isp_saturation_spv_len;
+extern const unsigned char g_isp_local_contrast_spv[];
+extern const int g_isp_local_contrast_spv_len;
+extern const unsigned char g_isp_unsharp_spv[];
+extern const int g_isp_unsharp_spv_len;
+extern const unsigned char g_isp_display_spv[];
+extern const int g_isp_display_spv_len;
+extern const unsigned char g_isp_algo_gamma_spv[];
+extern const int g_isp_algo_gamma_spv_len;
+extern const unsigned char g_isp_histogram_spv[];
+extern const int g_isp_histogram_spv_len;
 extern const unsigned char g_isp_aaf_spv[];
 extern const int g_isp_aaf_spv_len;
 extern const unsigned char g_isp_auto_contrast_spv[];
@@ -79,34 +107,6 @@ extern const unsigned char g_isp_yuv_sat_spv[];
 extern const int g_isp_yuv_sat_spv_len;
 extern const unsigned char g_isp_zone_stats_spv[];
 extern const int g_isp_zone_stats_spv_len;
-extern const unsigned char g_isp_normalize_spv[];
-extern const int g_isp_normalize_spv_len;
-extern const unsigned char g_isp_blc_spv[];
-extern const int g_isp_blc_spv_len;
-extern const unsigned char g_isp_cfa_spv[];
-extern const int g_isp_cfa_spv_len;
-extern const unsigned char g_isp_bayer_wb_spv[];
-extern const int g_isp_bayer_wb_spv_len;
-extern const unsigned char g_isp_demosaic_edge_spv[];
-extern const int g_isp_demosaic_edge_spv_len;
-extern const unsigned char g_isp_ccm_spv[];
-extern const int g_isp_ccm_spv_len;
-extern const unsigned char g_isp_downscale_spv[];
-extern const int g_isp_downscale_spv_len;
-extern const unsigned char g_isp_vignetting_spv[];
-extern const int g_isp_vignetting_spv_len;
-extern const unsigned char g_isp_saturation_spv[];
-extern const int g_isp_saturation_spv_len;
-extern const unsigned char g_isp_local_contrast_spv[];
-extern const int g_isp_local_contrast_spv_len;
-extern const unsigned char g_isp_unsharp_spv[];
-extern const int g_isp_unsharp_spv_len;
-extern const unsigned char g_isp_display_spv[];
-extern const int g_isp_display_spv_len;
-extern const unsigned char g_isp_algo_gamma_spv[];
-extern const int g_isp_algo_gamma_spv_len;
-extern const unsigned char g_isp_histogram_spv[];
-extern const int g_isp_histogram_spv_len;
 
 namespace MNN {
 
@@ -123,9 +123,9 @@ static const std::unordered_map<std::string, SpvData> g_ispSpvMap = {
     {"isp.awb",         {g_isp_awb_spv,          g_isp_awb_spv_len}},
     {"isp.calib_stats", {g_isp_calib_stats_spv,  g_isp_calib_stats_spv_len}},
     {"isp.denoise",     {g_isp_denoise_spv,      g_isp_denoise_spv_len}},
+    {"isp.eis_gyro",    {g_isp_eis_gyro_spv,     g_isp_eis_gyro_spv_len}},
     {"isp.ispc_stats",  {g_isp_ispc_stats_spv,   g_isp_ispc_stats_spv_len}},
     {"isp.tone",        {g_isp_tone_spv,         g_isp_tone_spv_len}},
-    {"isp.eis_gyro",    {g_isp_eis_gyro_spv,     g_isp_eis_gyro_spv_len}},
     {"isp.normalize",   {g_isp_normalize_spv,    g_isp_normalize_spv_len}},
     {"isp.blc",         {g_isp_blc_spv,          g_isp_blc_spv_len}},
     {"isp.cfa",         {g_isp_cfa_spv,          g_isp_cfa_spv_len}},
@@ -140,6 +140,31 @@ static const std::unordered_map<std::string, SpvData> g_ispSpvMap = {
     {"isp.display",     {g_isp_display_spv,      g_isp_display_spv_len}},
     {"isp.algo_gamma",  {g_isp_algo_gamma_spv,   g_isp_algo_gamma_spv_len}},
     {"isp.histogram",   {g_isp_histogram_spv,    g_isp_histogram_spv_len}},
+    {"isp.aaf",         {g_isp_aaf_spv,          g_isp_aaf_spv_len}},
+    {"isp.auto_contrast",{g_isp_auto_contrast_spv, g_isp_auto_contrast_spv_len}},
+    {"isp.bilateral",   {g_isp_bilateral_spv,    g_isp_bilateral_spv_len}},
+    {"isp.bnf",         {g_isp_bnf_spv,          g_isp_bnf_spv_len}},
+    {"isp.cct",         {g_isp_cct_spv,          g_isp_cct_spv_len}},
+    {"isp.ceh",         {g_isp_ceh_spv,          g_isp_ceh_spv_len}},
+    {"isp.chromatic_aberration",{g_isp_chromatic_aberration_spv, g_isp_chromatic_aberration_spv_len}},
+    {"isp.cnf",         {g_isp_cnf_spv,          g_isp_cnf_spv_len}},
+    {"isp.colorspace",  {g_isp_colorspace_spv,   g_isp_colorspace_spv_len}},
+    {"isp.demosaic_ccm",{g_isp_demosaic_ccm_spv, g_isp_demosaic_ccm_spv_len}},
+    {"isp.ee",          {g_isp_ee_spv,           g_isp_ee_spv_len}},
+    {"isp.fcs",         {g_isp_fcs_spv,          g_isp_fcs_spv_len}},
+    {"isp.gpu_warp",    {g_isp_gpu_warp_spv,     g_isp_gpu_warp_spv_len}},
+    {"isp.hdr_merge",   {g_isp_hdr_merge_spv,    g_isp_hdr_merge_spv_len}},
+    {"isp.ldci",        {g_isp_ldci_spv,         g_isp_ldci_spv_len}},
+    {"isp.lsc",         {g_isp_lsc_spv,          g_isp_lsc_spv_len}},
+    {"isp.nlm",         {g_isp_nlm_spv,          g_isp_nlm_spv_len}},
+    {"isp.pyramid",     {g_isp_pyramid_spv,      g_isp_pyramid_spv_len}},
+    {"isp.stats",       {g_isp_stats_spv,        g_isp_stats_spv_len}},
+    {"isp.temporal_denoise",{g_isp_temporal_denoise_spv, g_isp_temporal_denoise_spv_len}},
+    {"isp.tone_stats",  {g_isp_tone_stats_spv,   g_isp_tone_stats_spv_len}},
+    {"isp.warp",        {g_isp_warp_spv,         g_isp_warp_spv_len}},
+    {"isp.yuv_bcc",     {g_isp_yuv_bcc_spv,      g_isp_yuv_bcc_spv_len}},
+    {"isp.yuv_sat",     {g_isp_yuv_sat_spv,      g_isp_yuv_sat_spv_len}},
+    {"isp.zone_stats",  {g_isp_zone_stats_spv,   g_isp_zone_stats_spv_len}},
 };
 
 static inline SpvData lookupIspSpv(const std::string& opType) {
